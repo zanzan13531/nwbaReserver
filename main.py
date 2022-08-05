5c715f41e464b5180d1b09ad24899e1d45f7d912
zanzan13531 <zanzan13531@gmail.com>
2022-08-04 20:17:52 -0700

    finished writing the reserver, pretty much fully functional

--- main.py
+++ main.py
@@ -2,12 +2,30 @@ import json
 import requests
 from webbot import Browser
 
-zenPlannerCalendarPage = "https://northwestbadmintonacademy.sites.zenplanner.com/calendar.cfm"
-zenPlannerLoginPage = "https://northwestbadmintonacademy.sites.zenplanner.com/login.cfm"
-zenPlannerTestPage = "https://northwestbadmintonacademy.sites.zenplanner.com/calendar.cfm?DATE=2022%2D08%2D07&VIEW=LIST"
+zCalendarPage = "https://northwestbadmintonacademy.sites.zenplanner.com/calendar.cfm"
+zLoginPage = "https://northwestbadmintonacademy.sites.zenplanner.com/login.cfm"
+zTestPage = "https://northwestbadmintonacademy.sites.zenplanner.com/calendar.cfm?DATE=2022%2D08%2D07&VIEW=LIST"
+zUserProfilePage = "https://northwestbadmintonacademy.sites.zenplanner.com/person.cfm"
+zFamilyPage = "https://northwestbadmintonacademy.sites.zenplanner.com/family.cfm"
 
 urlDate = "?DATE="
 urlEnd = "&VIEW=LIST"
+urlCalendarPerson = "&calendarType=PERSON:"
+urlPerson = "&personId="
+
+def getUserEndlink(name, browser):
+
+    link = ""
+    browser.go_to(zFamilyPage)
+
+    if (browser.exists(name)):
+        browser.click(name)
+        link = browser.get_current_url()
+        return(link[link.index("=") + 1:])
+
+    else:
+        print("Family member not found: " + name + ".")
+
 
 username = ""
 password = ""
@@ -17,49 +35,59 @@ with open("login.json", "r") as f:
     username = data["username"]
     password = data["password"]
 
-
 web = Browser()
 
-print("Navigating to zenplanner")
+print("Navigating to zenplanner.")
 
-web.go_to(zenPlannerCalendarPage)
-web.go_to(zenPlannerLoginPage)
+web.go_to(zCalendarPage)
+web.go_to(zLoginPage)
 
 
 if (web.exists("Log In")):
     print("Not logged in, logging in...")
-    web.go_to(zenPlannerLoginPage)
+    web.go_to(zLoginPage)
     web.type(username , into='username', id='idUsername')
     web.type(password , into='Password' , id='idPassword')
     web.click('Log In' , tag='input')
 
 if (web.exists("My Profile")):
-    print("Successfully logged in")
+    print("Successfully logged in.")
+
 else:
-    print("An error occurred")
+    print("An error occurred.")
     quit()
 
-web.go_to(zenPlannerTestPage)
-web.click("8:00 PM")
-web.click("Reserve")
+
+commands = []
+
+with open("commands.json", "r") as f:
+    data = json.load(f)
+    commands = data["scheduled"]
+
+
+for x in commands:
+
+    userId = getUserEndlink(x["name"], web)
+    web.go_to(zCalendarPage + urlDate + x["date"] + urlEnd)
+
+    if (web.exists(x["time"])):
+        web.click(x["time"])
+
+        if (web.exists("All available spots for this class session are now taken.")):
+            print("Time slot found at " + x["time"] + " on " + x["date"] + " is full.")
+        
+        else:
+            web.go_to(web.get_current_url() + urlPerson + userId)
+            web.click("Reserve")
+
+            if (web.exists(x["name"] +  " is registered for this class.")):
+                print("Sucessfully registered " + x["name"] + " for the time slot found at " + x["time"] + " on " + x["date"])
+
+            else:
+                print("An error may have occurred when trying to register " + x["name"] + "for the time slot found at " + x["time"] + " on " + x["date"])
+
+    else:
+        print("No time slot found at " + x["time"] + " on " + x["date"])
+
 
 web.quit()
-
-
-# web.type('hello its me')  # or web.press(web.Key.SHIFT + 'hello its me')
-
-"""web.press(web.Key.ENTER)
-web.go_back()
-web.click('Sign in')
-web.type('mymail@gmail.com' , into='Email')
-web.click('NEXT' , tag='span')
-web.type('mypassword' , into='Password' , id='passwordFieldId')
-web.click('NEXT' , tag='span') # you are logged in . oohoooo
-
-
-zenPlannerMainPage = "https://northwestbadmintonacademy.sites.zenplanner.com/calendar.cfm"
-page = requests.get(zenPlannerMainPage)
-
-print(page.text)
-
-"""
